216b119feff5318b0296baa20510a502bef5faf2
zoloz966 <zrodrigo96@gmail.com>
2024-05-22 11:31:40 -0400

    we add all skeleton

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { CarouselModule } from 'primeng/carousel';
+import { DividerModule } from 'primeng/divider';
 
 export interface Products {
   name: string;
@@ -12,34 +13,65 @@ export interface Products {
   price: number;
 }
 
+export interface CurrentRetreats {
+  amount: number;
+  account_numb: string;
+  acount_name: string;
+  date: string;
+}
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RatingModule, FormsModule,CarouselModule],
+  imports: [
+    CommonModule,
+    DividerModule,
+    RatingModule,
+    FormsModule,
+    CarouselModule,
+  ],
   templateUrl: './home.component.html',
+  styles: `
+  .carousel-container {
+    display: flex;
+    overflow: hidden;
+  }
+
+  .carousel-item {
+    flex: 0 0 100%;
+  }
+
+  .dot {
+    width: 2px;
+    height: 2px;
+  }
+`,
 })
 export default class HomeComponent {
+  public carouselItems = [1, 2, 3];
+  public currentRetreats: CurrentRetreats[] = [];
+
   public productsA: Products[] = [
     {
       name: 'HAVIT HV-G92 Gamepad',
       photo: 'p1.png',
       commission: 27,
       rating: 3.5,
-      price: 120.00,
+      price: 120.0,
     },
     {
       name: 'AK-900 Wired Keyboard',
       photo: 'p2.png',
       commission: 25,
       rating: 3.8,
-      price: 56.50,
+      price: 56.5,
     },
     {
       name: 'IPS LCD Gaming Monitor',
       photo: 'p3.png',
       commission: 30,
       rating: 4,
-      price: 370.50,
+      price: 370.5,
     },
     {
       name: 'S-Series Comfort Chair',
@@ -56,30 +88,75 @@ export default class HomeComponent {
       photo: 'p5.png',
       commission: 15,
       rating: 4.8,
-      price: 260.00,
+      price: 260.0,
     },
     {
       name: 'Gucci duffle bag',
       photo: 'p6.png',
       commission: 18,
       rating: 5,
-      price: 960.20,
+      price: 960.2,
     },
     {
       name: 'RGB liquid CPU Cooler',
       photo: 'p7.png',
       commission: 23,
       rating: 4.5,
-      price: 160.50,
+      price: 160.5,
     },
     {
       name: 'Small BookSelf',
       photo: 'p8.png',
       commission: 20,
       rating: 5,
-      price: 360.30,
+      price: 360.3,
     },
   ];
 
-  constructor() {}
+  constructor() {
+    for (let i = 0; i < 9; i++) {
+      this.currentRetreats.push({
+        amount: this.generateRandomAmount(),
+        account_numb: this.generateRandomAccountNumber(),
+        acount_name: this.generateRandomAccountName(),
+        date: this.getCurrentDate(),
+      });
+    }
+    setInterval(() => {
+      this.currentRetreats.push({
+        amount: this.generateRandomAmount(),
+        account_numb: this.generateRandomAccountNumber(),
+        acount_name: this.generateRandomAccountName(),
+        date: this.getCurrentDate(),
+      });
+    }, 10200);
+  }
+
+  generateRandomAmount(): number {
+    const baseAmount = Math.random() * 2000 + 10;
+    const additionalAmount =
+      Math.random() * 60 * (Math.random() < 0.9 ? 1 : 15);
+    return +(baseAmount + additionalAmount).toFixed(2);
+  }
+
+  generateRandomAccountName(): string {
+    const accountNames = ['Skrill', 'Uphold', 'Airtm', 'Binance', 'AdvCash'];
+    return accountNames[Math.floor(Math.random() * accountNames.length)];
+  }
+
+  getCurrentDate(): string {
+    const today = new Date();
+    const day = today.getDate().toString().padStart(2, '0');
+    const month = (today.getMonth() + 1).toString().padStart(2, '0');
+    return `${day}/${month}`;
+  }
+
+  generateRandomAccountNumber(): string {
+    const providers = ['gmail', 'yahoo', 'hotmail', 'outlook', 'aol'];
+    const randomProvider =
+      providers[Math.floor(Math.random() * providers.length)];
+    const username = Math.random().toString(36).substr(2, 2);
+    const domain = randomProvider + '.com';
+    return `${username}********@${domain}`;
+  }
 }
